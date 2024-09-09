d42c1a5e343fffb5ada1d8acae00ae94408f89c0
umayr88 <islomovali6@gmail.com>
2024-09-09 09:20:02 +0200

    first inputs

---
+++ source/App.js
@@ -0,0 +1,14 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+
+
+function App() {
+  const [name, setName] = useState("");
+  const [list, setList] = useState(getLocalStorage());
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({
+    show: false,
+    msg: "",
+    type: "",
+  })};
